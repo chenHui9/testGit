0186e28340b27759f138f55fc39ffe502fb5c30b
chenhui <ch98542@163.com>
2019-06-21 17:37:01 +0800

    commit 6-21 test1

---
+++ src/main/java/test/GitProject.java
@@ -0,0 +1,11 @@
+package test;
+
+public class GitProject {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            System.out.println("hello git");
+        }
+
+    }
+
+}
