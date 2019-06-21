61d3e014f3a77407b2d41630030ffb97c5d81b9e
chenhui <ch98542@163.com>
2019-06-21 20:05:59 +0800

    chenhui2 6.21

--- src/main/java/test/GitProject.java
+++ src/main/java/test/GitProject.java
@@ -4,6 +4,7 @@ public class GitProject {
     public static void main(String[] args) {
         for (int i = 0; i < 10; i++) {
             System.out.println("hello git");
+            System.out.println("i am groot");
         }
 
     }
