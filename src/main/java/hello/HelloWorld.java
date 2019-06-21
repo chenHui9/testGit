30899b3d89334c795c4ef830b658e86ee1986d0d
chen <ch98542@163.com>
2019-06-21 21:14:09 +0800

    chenhui2 6.21.1

---
+++ src/main/java/hello/HelloWorld.java
@@ -0,0 +1,14 @@
+package hello;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 20; i++) {
+            int j = 0;
+            j++;
+            System.out.println(j);
+        }
+
+
+    }
+}
